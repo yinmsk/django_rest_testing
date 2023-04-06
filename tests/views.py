3c217d502c38e95899116b331d2c4bf8dbbf2f3e
yinmsk <galaxy15410@naver.com>
2023-04-06 13:17:05 +0900

    Create UserRegistration view for handling user registration in views.py

--- tests/views.py
+++ tests/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
 
 class MyTestingView(View):
     def get(self, request):
         return HttpResponse('Hello, World!')
+
+
+class UserRegistration(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
